48ab04b474f08d444906f2be71e3bdaea844a607
TYiFan <tyifan123@gmail.com>
2025-07-07 10:54:56 +0800

    Add user class

---
+++ backend/user.py
@@ -0,0 +1,14 @@
+from client import Client
+
+class User(Client):
+
+    def __init__(self):
+        super().__init__()
+    
+    def get_usage(self):
+        subscription = self.client.user.subscription.get()
+        remainder = subscription.character_limit - subscription.character_count
+        result = f"Character remaining: {remainder} characters"
+
+        print(result)
+        return result
