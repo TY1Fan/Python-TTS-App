bf711febab549b19ff21e1e75e60f1524db883a7
TYiFan <tyifan123@gmail.com>
2025-07-08 12:54:57 +0800

    Edit api screen to use OOP

--- frontend/screens/api_screen.py
+++ frontend/screens/api_screen.py
@@ -1,22 +1,24 @@
 import tkinter as tk
 
-def api_screen(client):
-    root = tk.Tk()
-    root.title("API Key Required")
+class Api_Screen:
+    
+    def __init__(self, client):
+        self.client = client
 
-    label = tk.Label(root, text="Enter Elevenlabs API Key:")
-    label.pack(pady=10)
+    def on_submit(self):
+        api_key = self.api_key_entry.get()
+        self.client.set_api_key(api_key)
 
-    api_key_entry = tk.Entry(root)
-    api_key_entry.pack(pady=10)
+    def display_screen(self):
+        self.root = tk.Tk()
+        self.root.title("API Key Required")
 
-    def on_submit():
-        api_key = api_key_entry.get()
-        client.set_api_key(api_key)
-        client.set_client()
-        root.destroy()
+        self.label = tk.Label(self.root, text="Enter Elevenlabs API Key:")
+        self.label.pack(pady=10)
 
-    submit_button = tk.Button(root, text="Submit", command=on_submit)
-    submit_button.pack(pady=10)
+        self.api_key_entry = tk.Entry(self.root)
+        self.api_key_entry.pack(pady=10)
 
-    root.mainloop()
+        self.submit_button = tk.Button(
+            self.root, text="Submit", command=self.on_submit)
+        self.submit_button.pack(pady=10)
