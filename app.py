39ef8c918ea2904b4410b3e11be34dfae2b42812
TYiFan <tyifan123@gmail.com>
2025-07-11 09:51:10 +0800

    Clean up app entry

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ from backend.voice import Voice
 from frontend.screens.api_screen import Api_Screen
 from frontend.screens.main_screen import Main_Screen
 
-##### Initialize modules #####
 client = Client()
 user = User()
 voice = Voice()
 home_screen = Main_Screen(client, user, voice)
 api_screen = Api_Screen(client, home_screen.display_screen)
 
-##### Frontend GUI setup #####
 if not os.path.isfile(".env"):
     client = api_screen.display_screen()
 else:
