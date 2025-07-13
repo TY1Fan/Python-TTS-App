dafca3542e2d010e656a71848c28cd6c87e49fa8
TYiFan <tyifan123@gmail.com>
2025-07-13 22:52:06 +0800

    Refactor voice class

--- backend/voice.py
+++ backend/voice.py
@@ -1,30 +1,18 @@
 import time
+import pygame
 
-from backend.client import Client
+from backend.character import Character
+from elevenlabs import VoiceSettings
 
-class Voice(Client):
+class Voice(Character):
 
     def __init__(self):
         super().__init__()
 
-    def get_voice_names(self):
-        response = self.client.voices.get_all()
-        voices = response.voices  # List of Voice objects
-
-        voice_dicts = []
-
-        for voice in voices:
-            voice_dicts.append({
-                voice.name
-            })
-
-        print(voice_dicts)
-        return voice_dicts
-
+    
     def generate_audio_from_text(self, model_id, voice_id, text, output_file, settings):
-
         start = time.time()
-
+        print(settings)
         audio = self.client.text_to_speech.convert(
             voice_id=voice_id,
             output_format="mp3_44100_128",
@@ -39,4 +27,35 @@ class Voice(Client):
 
         end = time.time()
         print(f"Audio generated in {end - start:.2f} seconds and saved to {output_file}")
+
+    def get_voice_settings(self, character_id):
+        settings = self.client.voices.settings.get(voice_id=character_id)
+        print(settings)
+        return settings
     
+    def update_voice_settings(self, character_id, updated_settings):
+        print(updated_settings)
+        self.client.voices.settings.update(
+            voice_id=character_id,
+            request=VoiceSettings(
+                stability=updated_settings.get("stability"),
+                use_speaker_boost=updated_settings.get("use_speaker_boost"),
+                similarity_boost=updated_settings.get("similarity_boost"),
+                style= updated_settings.get("style"),
+                speed= updated_settings.get("speed"),
+            ),
+        )
+        print(f"Settings updated for character ID {character_id}")
+
+    def play_audio(self, output_file_name):
+        pygame.mixer.init()
+        if output_file_name:
+            pygame.mixer.music.load(output_file_name)
+            pygame.mixer.music.play()
+
+    def stop_audio(self):
+        pygame.mixer.init()
+        pygame.mixer.music.stop()
+    
+
+
