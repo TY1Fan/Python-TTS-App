0af4ece9db2b73044b0b032792a78842c4c66ce3
TYiFan <tyifan123@gmail.com>
2025-07-13 22:50:50 +0800

    Integrate settings feature

--- frontend/screens/main_screen.py
+++ frontend/screens/main_screen.py
@@ -1,62 +1,90 @@
-import pygame
 import tkinter as tk
 
 from frontend.components.label import Label
 from frontend.components.entry import Entry
 from frontend.components.button import Button
 from frontend.components.textentry import TextEntry
+from frontend.components.dropdown import Dropdown
+from frontend.components.slider import Slider
+from frontend.components.checkbutton import Checkbutton
 
 class Main_Screen:
 
-    def __init__(self, client, user, voice):
+    output_file_name = "output.mp3"
+
+    def __init__(self, client, user, voice, character):
         self.client = client
         self.user = user
+        self.character = character
         self.voice = voice
         self.label = Label()
         self.entry = Entry()
         self.button = Button()
         self.text_entry = TextEntry()
-
-    def play_audio(self):
-        if output_file_name:
-            pygame.mixer.music.load(output_file_name)
-            pygame.mixer.music.play()
-
-    def stop_audio(self):
-        pygame.mixer.music.stop()
-
-    def generate_audio(self, text):
-        global output_file_name
-        if text:
-            model_id = "eleven_multilingual_v1"
-            voice_id = "onwK4e9ZLuTAKqWW03F9"
-            settings = None
-            output_file_name = "output.mp3"
-            self.voice.generate_audio_from_text(
-                model_id=model_id,
-                voice_id=voice_id,
-                text=text,
-                output_file=output_file_name,
-                settings=settings
-            )
-            print(f"Audio generated and saved to {output_file_name}")
-        else:
-            print("No text provided for audio generation.")
+        self.dropdown = Dropdown()
+        self.slider = Slider()
+        self.checkbutton = Checkbutton()
 
     def display_screen(self):
         root = tk.Tk()
-
         root.title("Python TTS App")
 
-        self.label.create_label(parent=root, text=self.user.get_usage(), pady=10)
-
-        command = lambda: self.generate_audio(text_entry.get("1.0", tk.END).strip())
+        char_names = self.character.get_char_names()
+        char_id = lambda: self.character.get_char_id(self.dropdown.widget.get())
+        generate_command = lambda: self.voice.generate_audio_from_text(
+            text = text_entry.get("1.0", tk.END).strip(), 
+            voice_id = char_id(),
+            output_file = self.output_file_name,
+            model_id = "eleven_multilingual_v2",
+            settings = self.voice.get_voice_settings(character_id=char_id())
+        )
         
-        self.button.create_button(root, text="Generate Audio", command=command, pady=10)
-        self.button.create_button(root, text="Play", command=self.play_audio, pady=10)    
-        self.button.create_button(root, text="Stop", command=self.stop_audio, pady=10)
-        text_entry = self.text_entry.create_text_entry(parent=root, height=5, width=50, pady=10)
+        self.label.create_label(parent=root, text=self.user.get_usage(), pady=10)
+        self.button.create_button(root, text="Generate Audio", command=generate_command, pady=10)
+        self.dropdown.create_dropdown(parent=root, values=char_names, pady=10)
+        settings = self.voice.get_voice_settings(character_id=char_id())
 
-        pygame.mixer.init()
+        settings_config = {
+            "stability": (0.0, 1.0, settings.stability),
+            "similarity_boost": (0.0, 1.0, settings.similarity_boost),
+            "style": (0.0, 1.0, settings.style),
+            "speed": (0.7, 1.2, settings.speed)
+        }
+
+        self.slider_values = {}
+
+        for name, (min_val, max_val, initial_val) in settings_config.items():
+            var = self.slider.create_slider(
+                parent=root,
+                name=name,
+                from_=min_val,
+                to=max_val,
+                initial=initial_val,
+                resolution=0.01,
+            )
+            self.slider_values[name] = var
+
+        self.use_boost_var = tk.BooleanVar(value=settings.use_speaker_boost)
+        self.checkbutton.create_checkbox(root, text="Use Speaker Boost", var=self.use_boost_var, pady=5)
+
+        self.button.create_button(
+            root,
+            text="Save Settings",
+            command=lambda: self.voice.update_voice_settings(
+                char_id(),
+                {
+                    "stability": self.slider_values["stability"].get(),
+                    "similarity_boost": self.slider_values["similarity_boost"].get(),
+                    "style": self.slider_values["style"].get(),
+                    "speed": self.slider_values["speed"].get(),
+                    "use_speaker_boost": self.use_boost_var.get()
+                }
+            ),
+            pady=10
+        )
+
+        self.button.create_button(root, text="Play", command=lambda: self.voice.play_audio(self.output_file_name), pady=10)    
+        self.button.create_button(root, text="Stop", command=lambda: self.voice.stop_audio(), pady=10)
+        text_entry = self.text_entry.create_text_entry(parent=root, height=5, width=50, pady=10)
 
         root.mainloop()
