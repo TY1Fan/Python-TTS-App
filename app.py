a12a86bc3239bcc4336bba30be8d405a51e0731f
TYiFan <tyifan123@gmail.com>
2025-07-13 22:51:42 +0800

    Update app with new backend module

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import os
 from backend.client import Client
 from backend.user import User
 from backend.voice import Voice
+from backend.character import Character
 from frontend.screens.api_screen import Api_Screen
 from frontend.screens.main_screen import Main_Screen
 
 client = Client()
 user = User()
+character = Character()
 voice = Voice()
-home_screen = Main_Screen(client, user, voice)
+home_screen = Main_Screen(client, user, voice, character)
 api_screen = Api_Screen(client, home_screen.display_screen)
 
 if not os.path.isfile(".env"):
