78b7d097759a5430c1fb3640f2d108e7ebbed2cd
TYiFan <tyifan123@gmail.com>
2025-07-14 20:51:10 +0800

    Update dropdown with var

--- frontend/components/dropdown.py
+++ frontend/components/dropdown.py
@@ -4,8 +4,8 @@ class Dropdown:
     def __init__(self):
         self.widget = None
 
-    def create_dropdown(self, parent, values, pady):
-        self.widget = ttk.Combobox(parent, values=values)
+    def create_dropdown(self, parent, values, pady, textvariable=None):
+        self.widget = ttk.Combobox(parent, values=values, textvariable=textvariable)
         self.widget.pack(pady=pady)
         self.widget.current(0)
         return self.widget
