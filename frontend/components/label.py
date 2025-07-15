18e41cd1fbef1928e25cab391617f014aeb15d7d
TYiFan <tyifan123@gmail.com>
2025-07-15 19:15:36 +0800

    Update label with font

--- frontend/components/label.py
+++ frontend/components/label.py
@@ -4,7 +4,7 @@ class Label:
     def __init__(self):
         None
     
-    def create_label(self, parent, text, pady):
-        label = tk.Label(parent, text=text)
+    def create_label(self, parent, text, pady, font=None):
+        label = tk.Label(parent, text=text, font=font)
         label.pack(pady=pady)
         return label
