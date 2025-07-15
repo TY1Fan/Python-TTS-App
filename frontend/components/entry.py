75e083b39979a10671d774d355a9eb7d1edeb95a
TYiFan <tyifan123@gmail.com>
2025-07-15 19:15:15 +0800

    Update entry method

--- frontend/components/entry.py
+++ frontend/components/entry.py
@@ -4,8 +4,8 @@ class Entry:
     def __init__(self):
         None
 
-    def create_entry(self, parent, pady):
-        entry = tk.Entry(parent)
+    def create_entry(self, parent, pady, width=20):
+        entry = tk.Entry(parent, width=width)
         entry.pack(pady=pady)
         return entry
     
