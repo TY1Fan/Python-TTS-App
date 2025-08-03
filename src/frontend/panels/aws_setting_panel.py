eeb3cc630801859d69c45c40bf2c5162ee446484
TYiFan <tyifan123@gmail.com>
2025-08-03 21:44:26 +0800

    Modify aws polly frontend

---
+++ src/frontend/panels/aws_setting_panel.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+
+from frontend.components.label import Label
+from frontend.components.button import Button
+from frontend.components.dropdown import Dropdown
+from frontend.components.slider import Slider
+from frontend.components.checkbutton import Checkbutton
+
+class AWS_Settings_Panel:
+
+    def __init__(self, client, voice, character):
+        self.client = client
+        self.character = character
+        self.voice = voice
+        self.label = Label()
+        self.button = Button()
+        self.dropdown = Dropdown()
+        self.slider = Slider()
+        self.checkbutton = Checkbutton()
+
+    def display_panel(self, parent):
+
+        self.selected_region_name = tk.StringVar()
+        self.selected_engine_name = tk.StringVar()
+        # self.selected_lang_code = tk.StringVar()
+        self.selected_char_name = tk.StringVar()
+
+        self.label.create_label(parent, "Select AWS Region:", pady=10)
+        self.region_dropdown = self.dropdown.create_dropdown(
+            parent, 
+            values=self.client.get_region(),
+            textvariable=self.selected_region_name, pady=10
+        )
+
+        engine = ["Standard", "Neural"]
+        self.label.create_label(parent, "Select Engine:", pady=10)
+        self.engine_dropdown = self.dropdown.create_dropdown(
+            parent, 
+            values=engine,
+            textvariable=self.selected_engine_name, pady=10
+        )
+
+        # lang_code = self.character.get_language_code(region_name=self.selected_region_name.get(), engine=self.selected_engine_name.get())
+        # self.label.create_label(parent, "Select Language Code:", pady=10)
+        # self.language_code_dropdown = self.dropdown.create_dropdown(
+        #     parent, 
+        #     values=lang_code,
+        #     textvariable=self.selected_lang_code, pady=10
+        # )
+
+        char_names = self.character.get_char_names(
+            engine=self.selected_engine_name.get().lower(),
+        )
+        self.label.create_label(parent, "Select Character:", pady=10)
+        self.char_dropdown = self.dropdown.create_dropdown(
+            parent, 
+            values=char_names,
+            textvariable=self.selected_char_name, pady=10
+        )
+
+    def get_char_id(self):
+        print(self.selected_char_name.get())
+        return self.character.get_char_id(self.selected_char_name.get())
+    
+
+
+
+
+
+
+        
--- src/frontend/screens/aws_screen.py
+++ src/frontend/screens/aws_screen.py
@@ -4,9 +4,9 @@ from frontend.components.label import Label
 from frontend.components.entry import Entry
 from frontend.components.button import Button
 from frontend.components.textentry import TextEntry
-from frontend.panels.settings_panel import Settings_Panel
+from frontend.panels.aws_setting_panel import AWS_Settings_Panel
 from frontend.panels.history_panel import History_Panel
-from frontend.panels.main_panel import Main_Panel
+from frontend.panels.aws_main_panel import AWS_Main_Panel
 
 class AWS_Screen:
 
@@ -21,37 +21,37 @@ class AWS_Screen:
         self.entry = Entry()
         self.button = Button()
         self.text_entry = TextEntry()
-        self.settings_panel = Settings_Panel(client, voice, character)
+        self.settings_panel = AWS_Settings_Panel(client, voice, character)
         self.history_panel = History_Panel(user, voice)
-        self.main_panel = Main_Panel(user, voice, self.output_file_name)
+        self.main_panel = AWS_Main_Panel(user, voice, self.output_file_name)
 
     def display_screen(self):
         root = tk.Tk()
         root.resizable(False, False)
         root.title("ThinkAloud")
 
-        # main_frame = tk.Frame(root)
-        # main_frame.pack(fill=tk.BOTH, expand=True)
+        main_frame = tk.Frame(root)
+        main_frame.pack(fill=tk.BOTH, expand=True)
 
-        # left_frame = tk.Frame(main_frame, padx=10, pady=10)
-        # left_frame.pack(side="left", fill=tk.Y)
+        left_frame = tk.Frame(main_frame, padx=10, pady=10)
+        left_frame.pack(side="left", fill=tk.Y)
 
-        # separator_left = tk.Frame(main_frame, width=2, bg="gray")
-        # separator_left.pack(side="left", fill=tk.Y)
+        separator_left = tk.Frame(main_frame, width=2, bg="gray")
+        separator_left.pack(side="left", fill=tk.Y)
 
-        # center_frame = tk.Frame(main_frame, padx=20, pady=20)
-        # center_frame.pack(side="left", fill=tk.BOTH, expand=True)
+        center_frame = tk.Frame(main_frame, padx=20, pady=20)
+        center_frame.pack(side="left", fill=tk.BOTH, expand=True)
 
-        # separator_right = tk.Frame(main_frame, width=2, bg="gray")
-        # separator_right.pack(side="left", fill=tk.Y)
+        separator_right = tk.Frame(main_frame, width=2, bg="gray")
+        separator_right.pack(side="left", fill=tk.Y)
 
-        # right_frame = tk.Frame(main_frame, padx=20, pady=20)
-        # right_frame.pack(side="left", fill=tk.BOTH, expand=True)
+        right_frame = tk.Frame(main_frame, padx=20, pady=20)
+        right_frame.pack(side="left", fill=tk.BOTH, expand=True)
 
-        # self.settings_panel.display_panel(right_frame)
+        self.settings_panel.display_panel(right_frame)
         # self.history_panel.display_panel(left_frame)
 
-        # char_id = self.settings_panel.get_char_id
-        # self.main_panel.display_card(center_frame, char_id)
+        char_id = self.settings_panel.get_char_id
+        self.main_panel.display_card(center_frame, char_id)
 
         root.mainloop()
