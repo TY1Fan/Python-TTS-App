e82dae56d101eac79c5bdd443044b400aa233b16
TYiFan <tyifan123@gmail.com>
2025-08-05 21:27:32 +0800

    Ensure that audio history folder exist

--- src/frontend/screens/aws_screen.py
+++ src/frontend/screens/aws_screen.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import os
+import sys
 
 from frontend.components.label import Label
 from frontend.components.entry import Entry
@@ -11,6 +13,16 @@ from frontend.panels.aws_main_panel import AWS_Main_Panel
 class AWS_Screen:
 
     output_file_name = "output.mp3"
+    if getattr(sys, 'frozen', False):
+        # Running in a bundle (PyInstaller)
+        base_path = os.path.dirname(sys.executable)
+    else:
+        # Running as a script
+        base_path = os.path.dirname(os.path.abspath(__file__))
+
+    # Path to the .env or credentials file in the same folder as the exe/script
+    history_path = os.path.join(base_path, "aws_audio")
+    os.makedirs(history_path, exist_ok=True)
 
     def __init__(self, client, user, voice, character):
         self.client = client
@@ -22,7 +34,7 @@ class AWS_Screen:
         self.button = Button()
         self.text_entry = TextEntry()
         self.settings_panel = AWS_Settings_Panel(client, voice, character)
-        self.history_panel = AWS_History_Panel(user, voice)
+        self.history_panel = AWS_History_Panel(user, voice, self.history_path)
         self.main_panel = AWS_Main_Panel(user, voice, self.output_file_name)
 
     def display_screen(self):
